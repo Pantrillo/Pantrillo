a76017b1ad0dedb9f45a04f643cee74b8751b4dd
Karl <99865019+krshinn@users.noreply.github.com>
2022-05-07 17:26:43 -0500

    Add files via upload

---
+++ src/pages/MealPlanner.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import MealList from "./MealList";
+import FoodJoke from "./FoodJoke";
+import './index.css';
+
+function MealPlanner() {
+  const [mealData, setMealData] = useState(null); //set to null default
+  const [calories, setCalories] = useState(2000); //set to 2000 default
+  
+  function handleChange(event) {
+    setCalories(event.target.value); //function listens for event to set calories
+  }
+  
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=dc278d39f926447f9f01c6c2a958f3be&timeFrame=day&targetCalories=${calories}`
+    ) //search meal plan by day and passing in state of calories
+    .then((response) => response.json())
+    .then((data) => {
+      setMealData(data);
+      console.log(data)
+    })
+    .catch(() => {
+      console.log("error")
+    });
+  }
+  
+  return (
+    <div className="App">
+
+      <section className="controls">
+        <input type="number" 
+        placeholder="Calories (e.g. 2000)" 
+        onChange={handleChange} />
+      </section>
+      <button onClick={getMealData}>Get Daily Meal Plan</button>
+      {mealData && <MealList mealData={mealData} />}
+
+    </div>
+  );
+}
+
+export default MealPlanner;
