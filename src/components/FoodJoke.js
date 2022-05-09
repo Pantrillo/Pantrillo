37051470a6536705ea3a505d5a93490102b04dd6
Karl <99865019+krshinn@users.noreply.github.com>
2022-05-09 17:55:56 -0500

    Add files via upload
    Added conditions to exclude jokes that exceed 150 characters.  Will now loop to acquire smaller jokes from API

--- src/components/FoodJoke.js
+++ src/components/FoodJoke.js
@@ -2,21 +2,35 @@ import React, {useState, useEffect} from 'react';
 
 function FoodJoke({data}) {
 	const [resultMsg, setResultMsg] = useState("Loading joke...");
+	
 	useEffect(() => {
 		fetch(
 			`https://api.spoonacular.com/food/jokes/random?apiKey=ec5faeee615c49b4a1af0ecb0c89bc0a`
 		)
 		.then((response) => response.json())
 		.then((data) => {
-			setResultMsg(data.text);
+			if(data.text.length < 150) {
+				setResultMsg(data.text);
+			}
+			else {
+				fetch(
+					`https://api.spoonacular.com/food/jokes/random?apiKey=ec5faeee615c49b4a1af0ecb0c89bc0a`
+				)
+				.then((response) => response.json())
+				.then((data) => {
+					setResultMsg(data.text);
 			console.log(data.text)
+		})
+	}
+
 		})
 		.catch(() => {
 			console.log("error");
 		})
 	}, [])
+	
 	return(
-		<span className="joke">
+		<span class="joke">
 			<p>{resultMsg}</p>
 		</span>
 	)
