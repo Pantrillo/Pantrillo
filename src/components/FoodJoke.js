e9f00c5068bddcacfbcbb070d577b9c716146ec0
Karl <99865019+krshinn@users.noreply.github.com>
2022-05-10 16:14:34 -0500

    reformatted joke component
    reformatted the joke function to an async in order to loop with a while-loop

--- src/components/FoodJoke.js
+++ src/components/FoodJoke.js
@@ -1,39 +1,37 @@
 import React, {useState, useEffect} from 'react';
 
-
-function FoodJoke({data}) {
+function FoodJoke() {
 	const [resultMsg, setResultMsg] = useState("Loading joke...");
-
+	
 	useEffect(() => {
-		fetch(
-			`https://api.spoonacular.com/food/jokes/random?apiKey=ec5faeee615c49b4a1af0ecb0c89bc0a`
-		)
-		.then((response) => response.json())
-		.then((data) => {
-			if(data.text.length < 125) {
-				setResultMsg(data.text);
-			}
-			else {
-				fetch(
-					`https://api.spoonacular.com/food/jokes/random?apiKey=ec5faeee615c49b4a1af0ecb0c89bc0a`
-				)
-				.then((response) => response.json())
-				.then((data) => {
-					setResultMsg(data.text);
-			console.log(data.text)
-		})
-	}
+		//fetch from the API
+		async function fetchJoke(){ 
+			const response = await fetch(
+			`https://api.spoonacular.com/food/jokes/random?apiKey=c88ec148e8da4e1085883c359737a08a`
+			)
+			const data = await response.json()
+			return data.text
+		}
+		//variable to send the data
+		let jokeString = ""
+		
+		//function to loop until you get a joke > 150 char
+		async function postJoke(){
+			while(jokeString === "" || jokeString.length > 150) {
+				jokeString = await fetchJoke()
+			}	
+			
+			//state set to variable
+			setResultMsg(jokeString);
+		}
+		postJoke()
 
-		})
-		.catch(() => {
-			console.log("error");
-		})
 	}, [])
-
+	
 	return(
-		<span className="joke">
+		<div className="joke">
 			<p>{resultMsg}</p>
-		</span>
+		</div>
 	)
 }
 
